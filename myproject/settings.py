31335a00766e48de3297115af36f67c67bb70217
Davy <crosse.shellac.0f@icloud.com>
2024-01-07 12:12:34 +0100

    Test params

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,8 +15,9 @@ import os
 
 PARAM1 = os.environ.get("PARAM1", "")
 PARAM2 = os.environ.get("PARAM2", "")
+PARAM3 = os.environ.get("PARAM3", "")
 
-print(f"Test params: {PARAM1=} {PARAM2=}")
+print(f"Test params: {PARAM1=} {PARAM2=} {PARAM3=}")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
